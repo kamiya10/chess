bb4627ab142467e7e2450a8060c82453871811a7
Kamiya <58339640+kamiya10@users.noreply.github.com>
2021-05-10 14:15:04 +0800

    small fix

--- 練習/Properties/AssemblyInfo.cs
+++ 練習/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("練習")]
+[assembly: AssemblyTitle("chess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("練習")]
+[assembly: AssemblyProduct("chess")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
